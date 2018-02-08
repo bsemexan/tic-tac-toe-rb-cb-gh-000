6aac5ecfeda529fb19ef2cf80524beebec40d844
Berry  Semexan <bsemexan@gmail.com>
2018-02-08 05:01:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,3 +25,13 @@ end
 def move(array, index, value = "X")
   array[index] = value
 end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  board[index] == "X" || board[index] == "O" ? true : false
+end
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8) ? true : false
+end
